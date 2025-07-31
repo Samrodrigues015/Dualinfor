37cb34f86949f009e96a0ea0a3c56caa038b1497
samarapaula2112@gmail.com <samarapaula2112@gmail.com>
2025-07-31 22:17:22 +0100

    Adicionando pagina home

--- functions.php
+++ functions.php
@@ -189,6 +189,9 @@ wp_enqueue_style('dualinfor-consultadoria-complementary-section-style', get_temp
 	// Estilo da página assistência técnica
 	wp_enqueue_style('dualinfor-pagina-assistenciatecnica-style', get_template_directory_uri() . '/assets/css/assistencia-tecnica.css', array(), $theme_version);
 
+	// Estilo da página assistência técnica
+	wp_enqueue_style('dualinfor-pagina-home-style', get_template_directory_uri() . '/assets/css/home.css', array(), $theme_version);
+
 	// JavaScript
 	wp_enqueue_script('dualinfor-main-js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), $theme_version, true);
 
